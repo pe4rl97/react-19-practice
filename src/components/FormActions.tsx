a16e3267029ac107aca6c03aa5c8a5c7378d6498
Pearl Patel <pearl@tag97.com>
2025-01-06 12:43:58 +0530

    feat: use of useOptimistic() and useFormStatus() hooks added

--- src/components/FormActions.tsx
+++ src/components/FormActions.tsx
@@ -1,5 +1,6 @@
-import { useActionState } from "react"
+import { useActionState, useOptimistic } from "react"
 import { updateNameInDB } from "../utils/api"
+import { useFormStatus } from 'react-dom'
 
 const FormActions = () => {
     const [state, actionFunction, isPending] = useActionState(
@@ -10,7 +11,10 @@ const FormActions = () => {
         }
     );
 
+    const [optimisticName, setOptimisticName] = useOptimistic(state.name);
+
     async function updateName(prevState, formData: FormData) {
+        setOptimisticName(formData.get('name'));
         try {
             const checkName = (formData.get('name') !== null) ? formData.get('name') : ''; 
             const newName = await updateNameInDB(checkName);
@@ -24,20 +28,30 @@ const FormActions = () => {
     return (
         <div className="container-md">
             <p className="username">
-                Current user: <span>{state.name}</span>
+                Current user: <span>{optimisticName}</span>
             </p>
-            {isPending && <p>Loading...</p>}
             <form action={actionFunction}>
                 <input
                     type="text"
                     name="name"
                     required
                 />
-                <button type="submit">Update</button>
+                <MyButton type="submit">Update</MyButton>
                 {!isPending && state.error && <p className="text-danger">{state.error.message}</p>}
             </form>
         </div>
     )
 }
 
+function MyButton({children, ...rest}) {
+    // const { pending: boolean, data: FormData, method: 'get' | 'post', action: actionFn()} = useFormStatus();
+    const { pending } = useFormStatus();
+
+    return (
+        <button {...rest}>{ pending ? 'Submitting....' : children }</button>
+    )
+}
+
 export default FormActions
+
+//another new feature that we can pass ref as prop any component as ref = {ref}
